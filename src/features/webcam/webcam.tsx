d132d9ac6b193cae036e6527148cf24d8cdb9eff
ngocminh180599 <ngocminhmtp123@gmail.com>
2021-01-06 14:06:41 +0700

    update

--- src/features/webcam/webcam.tsx
+++ src/features/webcam/webcam.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Webcam from "react-webcam";
 import "./webcam.css";
+import {URL_FACE_VIDEO} from '../../helper/base'
+import axios from "axios";
 
 interface Props {}
 interface State {
@@ -9,6 +11,8 @@ interface State {
   recording: boolean;
   recordedChunks: any;
   guiId: string;
+  file_video: any
+  token:string
 }
 
 class WebCam extends React.PureComponent<Props, State> {
@@ -20,12 +24,16 @@ class WebCam extends React.PureComponent<Props, State> {
       recording: false,
       recordedChunks: [],
       guiId: "",
+      file_video: null,
+      token:''
     };
   }
 
   componentDidMount() {
     const GUIID = localStorage.getItem("UserInfo_ guiId") || "";
-    this.setState({ guiId: GUIID });
+    const Token = localStorage.getItem("Token") || '';
+    
+    this.setState({ guiId: GUIID, token:Token });
   }
 
   webcamRef = React.createRef<Webcam>();
@@ -45,7 +53,8 @@ class WebCam extends React.PureComponent<Props, State> {
       this.webcamRef.current?.stream,
       {
         // mimeType: "video/webm;codecs=h264",
-        mimeType: "video/webm\;codecs=vp9",
+        mimeType: "video/webm;codecs=vp9",
+        // mimeType: "video/webm",
       }
     );
     this.mediaRecorderRef.current.addEventListener(
@@ -58,6 +67,7 @@ class WebCam extends React.PureComponent<Props, State> {
   /// --------------------------------------------------
   //@ts-ignore
   handleDataAvailable = ({ data }) => {
+    console.log('DATA :', data);
     if (data.size > 0) {
       this.setState({ recordedChunks: this.state.recordedChunks.concat(data) });
     }
@@ -68,24 +78,24 @@ class WebCam extends React.PureComponent<Props, State> {
   onStopRecord = () => {
     this.mediaRecorderRef.current?.stop();
     this.setState({ recording: false });
-    console.log("====================================");
-    console.log("RECORDS_CHUNKS : ", this.state.recordedChunks);
-    console.log("====================================");
+    // console.log("====================================");
+    // console.log("RECORDS_CHUNKS : ", this.state.recordedChunks);
+    // console.log("====================================");
 
-    const blob = new Blob(this.state.recordedChunks, {
-      type: "video/mp4",
-    });
-    const url = URL.createObjectURL(blob);
+    // const blob = new Blob(this.state.recordedChunks, {
+    //   type: "video/mp4",
+    // });
+    // const url = URL.createObjectURL(blob);
 
-    const file = new File(this.state.recordedChunks, "video.mp4", {
-      type: "video/mp4",
-    });
+    // const file = new File(this.state.recordedChunks, "video.mp4", {
+    //   type: "video/mp4",
+    // });
 
-    console.log('====================================');
-    console.log('Blob :', this.state.recordedChunks);
-    console.log("VIDEO_URL :", url);
-    console.log("FILE_VIEDO :", file);
-    console.log("====================================");
+    // console.log('====================================');
+    // console.log('Blob :', this.state.recordedChunks);
+    // console.log("VIDEO_URL :", url);
+    // console.log("FILE_VIEDO :", file);
+    // console.log("====================================");
   };
 
   // ----------------
@@ -95,6 +105,7 @@ class WebCam extends React.PureComponent<Props, State> {
         type: "video/mp4",
       });
       const url = URL.createObjectURL(blob);
+      
 
       const file = new File(this.state.recordedChunks, "video.mp4", {
         type: "video/mp4",
@@ -109,12 +120,15 @@ class WebCam extends React.PureComponent<Props, State> {
       const a = document.createElement("a");
       document.body.appendChild(a);
       // @ts-ignore
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam.mp4";
-      a.click();
-      window.URL.revokeObjectURL(url);
-      this.setState({ recordedChunks: [] });
+      // a.style = "display: none";
+      // a.href = url;
+      // a.download = "react-webcam.mp4";
+      // a.click();
+      // window.URL.revokeObjectURL(url);
+      // this.setState({ recordedChunks: [] });
+      this.playVideo();
+      this.verifyFace();
+       this.setState({ recordedChunks: [] });
     }
   };
 
@@ -151,9 +165,60 @@ class WebCam extends React.PureComponent<Props, State> {
 
   captureVideo = () => {
     this.onStartRecord();
-    setTimeout(this.onStopRecord, 3500);
+    // setTimeout(this.onStopRecord, 3500);
     // this.handleDownload();
   };
+
+  // 
+
+  playVideo=()=>{
+    var video = document.getElementById('preview-video');
+    const blob = new Blob(this.state.recordedChunks, {
+      type: "video/mp4",
+    });
+    const url = URL.createObjectURL(blob);
+    //@ts-ignore
+    video.src = url;
+    console.log('LINK_BLOB_VIDEO_PLAY: ', url);
+    
+      //@ts-ignore
+    video.load();
+      //@ts-ignore
+    video.onloadeddata = function() {
+        //@ts-ignore
+        video.play();
+    }
+  }
+
+  verifyFace = async () => {
+    var formData = new FormData();
+    const file = new File(this.state.recordedChunks, "video.mp4", {
+      type: "video/mp4",
+    });
+
+    formData.append(
+      "CaptureVideo",
+      file,
+      `CaptureVideo_${new Date().getTime()}.mp4`
+    );
+
+    formData.append('GuidID',this.state.guiId);
+
+    const config = {
+      headers: {
+        // "Content-Type": "application/json",
+        "content-type": "multipart/form-data",
+        Authorization: `Bearer ${this.state.token || ""}`,
+      },
+    };
+
+    await axios
+    .post(URL_FACE_VIDEO, formData, config)
+    .then((response) => {
+      console.log('RESPONSE : ', response);
+      
+    })
+  }
 
   render() {
     console.log("====================================");
@@ -176,6 +241,7 @@ class WebCam extends React.PureComponent<Props, State> {
           {this.renderDowloadBtn()}
         </div>
         <img src={this.state.screenshot} />
+        <video id = 'preview-video'></video>
       </div>
     );
   }
